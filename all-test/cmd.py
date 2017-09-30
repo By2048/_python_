29894f4dc87cb743d3f8ce9d61ae1faac1cb122a
AM <1247079575@qq.com>
2017-10-01 02:21:18 +0800

    md

---
+++ all-test/cmd.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import subprocess
+import command
+
+
+# def run_cmd(cmd):
+#     lines=[]
+#     # for line in os.popen(cmd).readlines():
+#     #     lines.append(line.replace('''\n''',''))
+#     with open(file_path, encoding='utf-8') as data_file:
+#         data_file.write(os.popen(cmd))
+#     # for line in lines:
+#     #     print(repr(line))
+
+
+if __name__=='__main__':
+
+    cmd_sr='node E:\\Desktop\\MarkDownToHtml\\make.js'
+    # out_put=os.system(cmd_sr)
+
+    # output = os.popen('cat /proc/cpuinfo')
+    # print(output.read())
+
+    # print(out_put.read())
+    file_path = "f:\\keep.txt"
+    # with open(file_path,'w') as file:
+    #     file.write(os.system(cmd_sr))
+
+    # tmps = os.popen(cmd_sr).readlines()
+    # for tmp in tmps:
+    #     print(tmp, end='')
+    # utf8  gbk encode  decode
+    # run_cmd('chcp 65001')
+    # run_cmd(cmd_sr)
+
+    # p = os.popen("node E:\\Desktop\\MarkDownToHtml\\make.js", 'r')
+    # print(p.read())
+
+    pp=subprocess.check_output(cmd_sr)
+    with open(file_path,'wb') as file:
+        file.write(pp)
