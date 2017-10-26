48acdb6053d9fecf4b7237e47d32c35870b37b85
AM <1247079575@qq.com>
2017-10-26 08:45:40 +0800

    bug fix

--- bilibili/_test_open_js.py
+++ bilibili/_test_open_js.py
@@ -5,50 +5,38 @@ import time
 import math
 import sys
 import re
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-try:
-    from config import *
-    from download import *
-    from sql import *
-    from tool import *
-    from image import *
-except ImportError:
-    from .config import *
-    from .download import *
-    from .sql import *
-    from .tool import *
-    from .image import *
+from selenium.webdriver.common.action_chains import ActionChains
+import selenium.webdriver as webdriver
+# import selenium.webdriver.support.ui as ui
+# from selenium.webdriver.common.keys import Keys
+
+
+chrome_path = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 
 links=[
-"http://h.bilibili.com/749728",
-"http://h.bilibili.com/752736",
-"http://h.bilibili.com/740834",
-"http://h.bilibili.com/765719",
-"http://h.bilibili.com/755273",
-"http://h.bilibili.com/13248",
-"http://h.bilibili.com/758520",
-"http://h.bilibili.com/761004",
-"http://h.bilibili.com/666859",
-"http://h.bilibili.com/761224",
-"http://h.bilibili.com/747787",
-"http://h.bilibili.com/760043",
-"http://h.bilibili.com/706161",
-"http://h.bilibili.com/546947",
-"http://h.bilibili.com/683404",
-"http://h.bilibili.com/687107",
-"http://h.bilibili.com/768633",
-"http://h.bilibili.com/748118",
-"http://h.bilibili.com/765654",
-"http://h.bilibili.com/691685"
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com",
+"http://www.baidu.com"
 ]
 
 
 chrome = webdriver.Chrome(chrome_path)
-chrome.get("http://www.baidu.com")
+# geckodriver
+# chrome = webdriver.Firefox()
+# chrome.get("http://www.baidu.com")
 
 for link in links:
     print(link)
@@ -56,16 +44,15 @@ for link in links:
 def run_js(chrome,jss):
     for js in jss:
         print(js)
-        chrome.execute_script(js,"")
+        chrome.execute_script(js)
     print('run over -----')
+    time.sleep(1)
 
 def refresh_handles():
     return chrome.window_handles
 
 setp = 3
 for i in range(0,len(links),setp):
-
-    print(chrome.current_url)
 
     jss = []
     for link in links[i:i + setp]:
@@ -74,20 +61,23 @@ for i in range(0,len(links),setp):
         jss.append(js)
     run_js(chrome,jss)
 
-    handles = chrome.window_handles
+    handles= chrome.window_handles
 
     print('1 --- ',end='')
-    print(handles)
+    print("\n".join(handles))
 
     for handle in handles[1:]:
         chrome.switch_to.window(handle)
-        # ActionChains(chrome).send_keys(Keys.CONTROL+'t').perform()
+        time.sleep(1)
         chrome.close()
+        # handles=chrome.window_handles
 
+    chrome.switch_to_window(handles[0])
     handles=chrome.window_handles
 
-    WebDriverWait(chrome, 10).until(lambda d: len(d.window_handles) == 1)
+    # WebDriverWait(chrome, 10).until(lambda d: len(d.window_handles) == 1)
     print('2 --- ',end='')
-    print(handles)
+    print("\n".join(handles))
 
-    time.sleep(3)
+
+    time.sleep(5)
