52203edd761f74ff91355e9ef25c65cb107fb66e
AM <1247079575@qq.com>
2017-12-06 20:05:11 +0800

    细节修改

--- ass-change-language/main.py
+++ ass-change-language/main.py
@@ -1,36 +1,33 @@
 import os
+import shutil
 from hanziconv import HanziConv
 
-start_path=r'E:\Desktop\subs test'
-change_path=r'E:\Desktop\subs change'
+# 需要转换的字幕文件夹的位置
+sub_path= r'E:\Desktop\Subs'
 
+# 转换后的文件夹的位置
+change_sub_path = r'E:\Desktop\Subs_Change'
 
-def change_ass(old_path):
-    new_path=change_path+'\\'+os.path.basename(old_path)
+# 创建文件夹
+def create_folder():
+    if not os.path.isdir(change_sub_path):
+        os.mkdir(change_sub_path)
+    if not os.path.isdir(sub_path):
+        os.mkdir(sub_path)
 
-    old_file = open(old_path, mode='r', encoding='utf-8-sig')
-    new_file=open(new_path,mode='w',encoding='utf-8-sig')
+# 进行转换
+def change_ass(old_file_path,new_file_path):
+    old_file = open(old_file_path, mode='r', encoding='utf-8-sig')
+    new_file=open(new_file_path,mode='w',encoding='utf-8-sig')
 
     for line in old_file.readlines():
         new_line=HanziConv.toSimplified(line)
         new_file.write(new_line)
 
 
-
-
-# file_path=r'E:\Desktop\1.ass'
-#
-# file_path_new=r'E:\Desktop\2.ass'
-#
-# file=open(file_path,mode='r',encoding='utf-8-sig')
-#
-# file_new=open(file_path_new,mode='w',encoding='utf-8-sig')
-#
-# for line in file.readlines():
-#     new_line=HanziConv.toSimplified(line)
-#     file_new.write(new_line)
-
-
-for path in os.listdir(start_path):
-    old_path=start_path+'\\'+path
-    change_ass(old_path)
+if __name__=='__main__':
+    create_folder()
+    for path in os.listdir(sub_path):
+        old_file_path = sub_path + '\\' + path
+        new_file_path = change_sub_path + '\\' + os.path.basename(path)
+        change_ass(old_file_path,new_file_path)
