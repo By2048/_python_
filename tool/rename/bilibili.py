1c1267a4b70296f1f98b38e10a539c9cde50be4e
AM <1247079575@qq.com>
2018-02-13 12:19:36 +0800

    bilibili
    Signed-off-by: AM <1247079575@qq.com>

--- tool/rename/bilibili.py
+++ tool/rename/bilibili.py
@@ -1,4 +1,5 @@
 import os, json, shutil
+from hanziconv import HanziConv
 
 # download
 # ├─13601266
@@ -29,17 +30,39 @@ class BVideo():
         print(self.name, end='\t')
         print()
 
-def get_video_name(_path,_num):
+
+def get_video_name(_path, _num):
     json_path = os.path.join(_path, 'entry.json')
     with open(json_path, 'r', encoding='utf-8') as json_file:
         json_data = json.load(json_file)
         title = json_data['title']
-        if _num == 1:
-            page_data = ''
-        else:
-            page_data = '_' + json_data['page_data']['part']
-        video_name = title + page_data
-    return video_name
+        try:
+            if json_data['page_data'] != None:
+                if _num == 1:
+                    page_data = ''
+                else:
+                    page_data = '_' + json_data['page_data']['part']
+                video_name = title + page_data
+                video_name=change_name(video_name)
+                return video_name
+        except KeyError:
+            if json_data['ep'] != None:
+                if _num == 1:
+                    index = json_data['ep']['index']
+                    index_title = json_data['ep']['index_title']
+                    if index_title == "":
+                        video_name = title + '_' + index
+                    else:
+                        video_name = title + '_' + index + '_' + index_title
+                else:
+                    index = json_data['ep']['index']
+                    index_title = json_data['ep']['index_title']
+                    if index_title == "":
+                        video_name = title + '_' + index
+                    else:
+                        video_name = title + '_' + index + '_' + index_title
+            video_name=change_name(video_name)
+            return video_name
 
 
 def get_all_bvideo():
@@ -48,11 +71,11 @@ def get_all_bvideo():
         for page_fodler in os.listdir(av_path):
             _num = len(os.listdir(av_path))
             _path = os.path.join(av_path, page_fodler)
-            _name = get_video_name(_path,_num)
+            _name = get_video_name(_path, _num)
             bvideo = BVideo(num=_num, path=_path, name=_name)
             all_bvideo.append(bvideo)
-
             bvideo.show()
+
 
 def move_blv():
     for video in all_bvideo:
@@ -62,6 +85,7 @@ def move_blv():
                     shutil.move(os.path.join(root, file), video.path)
                 else:
                     pass
+
 
 def blv_to_flv():
     for video in all_bvideo:
@@ -74,7 +98,6 @@ def blv_to_flv():
                 pass
 
 
-
 # 在每个视频文件夹路径下获取 video_list.txt
 def get_video_list():
     file_line = []
@@ -82,40 +105,44 @@ def get_video_list():
         for file in os.listdir(video.path):
             if file.endswith(('.flv')):
                 file_line.append(os.path.join(video.path, file))
-        file_line.sort(key=lambda x: int(os.path.basename(x).split('.')[0]),reverse=False)
+        file_line.sort(key=lambda x: int(os.path.basename(x).split('.')[0]), reverse=False)
         video_list_path = os.path.join(video.path, 'video_list.txt')
         with open(video_list_path, 'w+', encoding='utf-8') as file:
             for line in file_line:
                 file.write('file' + ' ' + '\'' + line + '\'')
                 file.write('\n')
 
-# 使用 ffmpeg 拼接视频
 
+# 使用 ffmpeg 拼接视频
 def get_sum_video():
     for video in all_bvideo:
-        video_list_path=os.path.join(video.path,'video_list.txt')
-        new_video_path = os.path.join(new_path, video.name)+ '.flv'
-
+        video_list_path = os.path.join(video.path, 'video_list.txt')
+        new_video_path = os.path.join(new_path, video.name) + '.flv'
         cmd = "{0} -f concat -safe 0 -i {1} -c copy {2}".format(ffmpeg_path, video_list_path, new_video_path)
         print(cmd + '\n')
         os.system(cmd)
+
 
 def change_name(old_name):
     new_name = old_name \
         .replace('?', '') \
         .replace('？', '') \
-        .replace('【', '[') \
-        .replace('】', ']') \
         .replace('| ', '') \
-        .replace('*', '')
+        .replace('*', '')\
+        .replace(' ','_')
+# .replace('【', '[') \
+# .replace('】', ']')
+# .replace('《','')\
+# .replace('》','')\
+# .replace('，','')\
     new_name = HanziConv.toSimplified(new_name)
-    print(new_name)
     return new_name
 
 def _test():
-    _path='..\\lib\\ffmpeg.exe'
+    _path = '..\\lib\\ffmpeg.exe'
     print(os.path.abspath(_path))
     print(os.getcwd())
+
 
 if __name__ == '__main__':
     get_all_bvideo()
@@ -124,5 +151,4 @@ if __name__ == '__main__':
     get_video_list()
     get_sum_video()
 
-    _test()
-
+    # _test()
