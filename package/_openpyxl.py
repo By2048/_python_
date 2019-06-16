8db7f4a5a548fadc298c1722dda20bb8bc0f7d4f
By2048 <1247079575@qq.com>
2019-06-16 21:10:18 +0800

    package excel

---
+++ package/_openpyxl.py
@@ -0,0 +1,32 @@
+import logging
+
+import base
+
+from openpyxl import load_workbook
+from openpyxl import Workbook
+
+wb = load_workbook('_openpyxl.xlsx')
+
+
+def test_read():
+    def test_1():
+        for name in wb.sheetnames:
+            for row in wb[name].rows:
+                logging.info([col.value for col in row])
+
+    def test_2():
+        for name in wb.sheetnames:
+            for col in wb[name].columns:
+                logging.info([row.value for row in col])
+
+    test_1()
+    test_2()
+
+
+def test_write():
+    # TODO write excel
+    pass
+
+
+if __name__ == '__main__':
+    test_read()
