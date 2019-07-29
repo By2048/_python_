81a95f56748ce04f9f3c7c9b542987da8ca47f71
By2048 <1247079575@qq.com>
2019-07-29 10:25:19 +0800

    .
    Signed-off-by: By2048 <1247079575@qq.com>

--- all-test/multiprocessing-test/thread_lock.py
+++ all-test/multiprocessing-test/thread_lock.py
@@ -9,12 +9,16 @@
 import time, threading
 
 balance = 0
+
+
 def change_it(n):
     # 先存后取，结果应该是0
     global balance
     balance = balance + n
     balance = balance - n
     print(balance)
+
+
 """
 # 假定这是你银行的存款
 
@@ -43,6 +47,8 @@ def run_thread(n):
 """
 
 lock = threading.Lock()
+
+
 def run_thread(n):
     for i in range(100000):
         # 先要获取锁:
@@ -53,6 +59,8 @@ def run_thread(n):
         finally:
             # 改完了一定要释放锁:
             lock.release()
+
+
 t1 = threading.Thread(target=run_thread, args=(5,))
 t2 = threading.Thread(target=run_thread, args=(8,))
 t1.start()
@@ -60,6 +68,3 @@ t2.start()
 t1.join()
 t2.join()
 print(balance)
-
-
-
