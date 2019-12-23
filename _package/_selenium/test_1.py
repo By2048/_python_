609053d96f4aa428e6faf917684b400e8e3cbdea
By2048 <dojogh12>
2019-12-23 21:03:44 +0800

    update project

--- _package/_selenium/test_1.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from selenium import webdriver
-
-dev = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
-
-chrome = webdriver.Chrome(executable_path=dev)
-time.sleep(5)
-chrome.quit()
