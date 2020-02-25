d32a0ea258a6acbd095ac056080bd754d7a46991
By2048 <dojogh12>
2020-02-25 16:15:08 +0800

    update redis test

--- _sql_/_redis_/_base_.py
+++ _sql_/_redis_/_base_.py
@@ -45,22 +45,35 @@ def _exists_():
     result = db.exists('key|none')
     assert result == 0
 
-    db.set('key|exists', 'value|exists')
+    db.set('key|exists|1', 'value|exists|1')
+    db.set('key|exists|2', 'value|exists|2')
+
     result = db.exists('key|exists')
     assert result == 1
 
     result = db.exists('key|none|1', 'key|none|2')
     assert result == 0
 
-    result = db.exists('key|none|1', 'key|none|2', 'key|exists')
+    result = db.exists('key|exists|1', 'key|exists|2', 'key|none')
+    assert result == 2
+
+
+def _delete_():
+    result = db.delete('key|none')
+    assert result == 0
+
+    db.set('key|delete', 'value|delete')
+    result = db.delete('key|delete')
     assert result == 1
 
+    db.set('key|delete|1', 'value|delete|1')
+    db.set('key|delete|2', 'value|delete|1')
 
-def _list_():
-    db.delete('key3')
-    db.rpush('key3', 1)
-    db.rpush('key3', 2, 3, json.dumps([1, 2, 3]))
-    logging.info(db.lrange('key3', 0, -1))
+    result = db.delete('key|none|1', 'key|none|2')
+    assert result == 0
+
+    result = db.delete('key|delete|1', 'key|delete|2', 'key|none')
+    assert result == 2
 
 
 if __name__ == '__main__':
@@ -69,3 +82,4 @@ if __name__ == '__main__':
     _get_()
     _expire_()
     _exists_()
+    _delete_()
