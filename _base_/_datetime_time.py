d3a588187a66aa696240a4859ebcf7467f879c47
By2048 <dojogh12>
2020-03-20 16:53:25 +0800

    datetime and time

--- _base_/_datetime_time.py
+++ _base_/_datetime_time.py
@@ -1,12 +1,9 @@
-import logging
 from datetime import datetime
+import time
 
-data = 1273969203.1234
-
-date = datetime.fromtimestamp(data)
-logging.info([date, type(date)])
-
-date = datetime.utcfromtimestamp(data)
-logging.info([date, type(date)])
-
-logging.info(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print(1, datetime.fromtimestamp(1273969203.1234))
+print(2, datetime.utcfromtimestamp(1273969203.1234))
+print(3, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print(4, datetime.strptime('2016-08-26', '%Y-%m-%d'))
+print(5, time.mktime(datetime.now().timetuple()))
+print(6, time.strftime('%Y-%m-%d', time.localtime(datetime.now().timestamp())))
