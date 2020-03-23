6f1b003b4ed4a0fb6cebfc6be49fc42dd6b9cc07
By2048 <1247079575@qq.com>
2020-03-23 18:48:49 +0800

    update bilibili tool

--- _demo_/_bilibili_free_video_.py
+++ _demo_/_bilibili_free_video_.py
@@ -17,6 +17,7 @@ url = 'https://api.bilibili.com/pgc/season/index/result' \
       '&pagesize=50' \
       '&type=1'
 
+
 # order
 # 0 更新时间
 # 1 无
@@ -25,14 +26,21 @@ url = 'https://api.bilibili.com/pgc/season/index/result' \
 # 4 最高评分
 # 5 开播时间
 
-for page in range(1, 99):
-    response = requests.get(url.format(page))
-    response = response.json()
-    data = response.get('data')
-    if not data.get('has_next'):
-        print(f'\n max page {page} \n')
-        break
-    for video in data.get('list'):
-        badge = video.get('badge')
-        if badge in ['限时免费']:
-            print(f"{video['title']:<20} | {video['order']:<15} | {video['link']}")
+def main():
+    for page in range(1, 99):
+        response = requests.get(url.format(page))
+        response = response.json()
+        data = response.get('data')
+
+        for video in data.get('list'):
+            badge = video.get('badge')
+            if badge in ['限时免费']:
+                print(f"{video['title']:>30} | {video['link']}")
+
+        if not data.get('has_next'):
+            break
+
+
+if __name__ == '__main__':
+    print()
+    main()
