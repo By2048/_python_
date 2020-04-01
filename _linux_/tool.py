9329c96b8a2ff9e403f510fd961fce173983be76
By2048 <1247079575@qq.com>
2020-04-01 14:10:56 +0800

    update tool

--- _linux_/tool.py
+++ _linux_/tool.py
@@ -3,23 +3,26 @@
 import os
 import shutil
 
-# 直接进行脚本复制
-path_tool = '/root/deployment/_python_/_linux_'
-path_bin = '/root/bin'
+path_src = '/root/deployment/_python_/_linux_'
+path_dsc = '/root/bin'
 
-if os.path.exists(path_tool) and os.path.exists(path_bin):
-    shutil.rmtree(path_bin)
-    shutil.copytree(path_tool, path_bin)
+for item in os.listdir(path_dsc):
+    file_dst = os.path.join(path_dsc, item)
+    if not os.path.exists(file_dst):
+        continue
+    os.remove(file_dst)
 
-path_init_py = os.path.join(path_bin, '__init__.py')
-if os.path.isfile(path_init_py):
-    os.remove(path_init_py)
+for item in os.listdir(path_src):
+    file_src = os.path.join(path_src, item)
+    file_dst = os.path.join(path_dsc, item).rstrip('.py')
 
-# 脚本文件上传后内容修改
-for item in os.listdir(path_bin):
-    if item.endswith('.py'):
-        _origin = os.path.join(path_bin, item)
-        _change = _origin.replace('.py', '')
-        os.rename(_origin, _change)
-        os.system(f"chmod +x {_change}")
-        os.system(f"dos2unix {_change}")  # fix python ^M error
+    if os.path.exists(file_dst):
+        os.remove(file_dst)
+
+    if item in ['__init__.py']:
+        continue
+
+    shutil.copy(file_src, file_dst)
+
+    os.system(f"chmod +x {file_dst}")
+    os.system(f"dos2unix {file_dst}")  # fix python ^M error
