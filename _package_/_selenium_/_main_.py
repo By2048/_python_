86256eef9a223b73986d510ef28a3afc6e643d8f
By2048 <1247079575@qq.com>
2020-05-26 19:19:12 +0800

    update selenium

--- _package_/_selenium_/_main_.py
+++ _package_/_selenium_/_main_.py
@@ -7,14 +7,14 @@ chrome_driver = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.
 
 chrome = webdriver.Chrome(executable_path=chrome_driver)
 
-# chrome.set_window_size(100, 300)
+chrome.set_window_size(100, 300)
 
 chrome.get('http://www.baidu.com')
-# source = chrome.page_source
-# pprint(source)
 
+source = chrome.page_source
+pprint(source)
 
 time.sleep(3)
 
 chrome.close()
-# chrome.quit()
+chrome.quit()
