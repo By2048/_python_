afce2bb1f7784ae4d757347c0df6fa3978585cd1
By2048 <1247079575@qq.com>
2020-08-06 20:30:51 +0800

    update 重构重命名脚本

---
+++ _script_/_bilibili_rename_.py
@@ -0,0 +1,46 @@
+import re
+import os
+
+path = ''  # 文件夹路径
+videos = []  # 视频文件名
+
+try:
+    from ._rename_ import File, init_files_info, print_files_info, rename_files
+except ImportError:
+    from _rename_ import File, init_files_info, print_files_info, rename_files
+
+
+def init():
+    print("\n粘贴文件夹路径\n")
+    global path
+    path = input()
+    path = path.strip('"')
+
+    if os.path.isfile(path):
+        file = File()
+        file.folder = os.path.dirname(path)
+        file.old_name = os.path.basename(path)
+        videos.append(file)
+
+    for item in os.listdir(path):
+        file = File(folder=path, old_name=item)
+        videos.append(file)
+
+
+def get_name(old_name):
+    item = old_name.split('.')
+    try:
+        index, name, file_type = item[0].zfill(2), item[1], item[2]
+    except Exception as e:
+        print(f'\n文件名解析错误 {old_name}\n')
+        return False
+    video_title = ''.join(re.split(r'\([avAVpP,\d]+\)', name))  # 去除(Avxxxxxx,Pxxxxx)
+    new_name = f"{index} {video_title}.{file_type}"
+    return new_name
+
+
+if __name__ == '__main__':
+    init()
+    init_files_info(videos, get_name)
+    print_files_info(videos)
+    rename_files(videos)
---
+++ _script_/_rename_.py
@@ -0,0 +1,51 @@
+import os
+
+from typing import List
+
+
+class File(object):
+    def __init__(self, folder='', old_name='', new_name=''):
+        self.folder = folder
+        self.old_name = old_name
+        self.new_name = new_name
+
+    def __str__(self):
+        return f"{self.old_name} \t {self.new_name}"
+
+
+def init_files_info(files: List[File], function):
+    for file in files:
+        file.new_name = function(file.old_name)
+
+
+def print_files_info(files: List[File]):
+    try:
+        from veryprettytable import VeryPrettyTable
+    except ImportError:
+        for file in files:
+            print(file)
+        return
+    table = VeryPrettyTable(['old_name', 'new_name'])
+    table.align['old_name'] = 'l'
+    table.align['new_name'] = 'l'
+    for file in files:
+        table.add_row([file.old_name, file.new_name])
+    print(table)
+
+
+def rename_files(files: List[File]):
+    print('\n确认重命名\n')
+    check = input()
+
+    if check not in ('1', 'true', 'y', True) or not check:
+        return
+
+    for file in files:
+        if not file.old_name or not file.new_name:
+            print(f'获取文件名错误\n{file}')
+            return
+
+    for file in files:
+        old = os.path.join(file.folder, file.old_name)
+        new = os.path.join(file.folder, file.new_name)
+        os.rename(old, new)
