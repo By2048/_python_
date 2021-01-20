e79d9835568609799c6ac90d770b6f26f2bd839c
By2048 <1247079575@qq.com>
2021-01-20 17:44:27 +0800

    update _sort_

---
+++ _note_/_sort_.py
@@ -0,0 +1,51 @@
+from random import shuffle
+from functools import cmp_to_key
+
+from _tool_._decorator_ import log, run_all_test
+
+
+def key(item):
+    return len(item)
+
+
+def cmp(a, b):
+    return len(a) - len(b)
+
+
+data = ["1", "1, 2, 3", "1, 2", "1, 2, 3, 4"]
+
+
+@log
+def test_1():
+    global data
+    shuffle(data)
+    data = sorted(data, key=cmp_to_key(cmp))
+    return data
+
+
+@log
+def test_2():
+    global data
+    shuffle(data)
+    data.sort(key=cmp_to_key(cmp))
+    return data
+
+
+@log
+def test_3():
+    global data
+    shuffle(data)
+    data = sorted(data, key=key)
+    return data
+
+
+@log
+def test_4():
+    global data
+    shuffle(data)
+    data.sort(key=key)
+    return data
+
+
+if __name__ == '__main__':
+    run_all_test(globals())
