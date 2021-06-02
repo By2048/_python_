b2ca313bc081235394cd1015b77074bad0b0f2dd
By2048 <1247079575@qq.com>
2021-06-02 19:00:34 +0800

    remove _notebook_

--- _demo_/_notebook_/config.py
+++
@@ -1,7 +0,0 @@
-note_book_path = 'E:\\MyGit\\NoteBook'
-
-cmd_keep_path = note_book_path + '\\.cmd'
-
-ignore_file = ['.json', '.pdf', '.mhtml', 'README.md', 'SUMMARY.md']
-
-ignore_folder = ['.code', '.git', '.gitignore', '.cmd', '.vscode', 'HtmlKeep', '_code', '_image']
